e3d70e2e3a6c400df6c511c25647e81864c49b47
eeitobi <38220676+eeitobi@users.noreply.github.com>
2023-01-25 00:06:46 +0100

    Objectification started Strava API handling in strava_connector Import logic in strava_import_handler --> tbd

---
+++ strava_import_handler.py
@@ -0,0 +1,11 @@
+import strava_connector as sc
+
+class StravaImportHandler():
+    """
+    TODO: docstring
+    """
+    def __init__(self) -> None:
+        pass
+
+    def import_all_available() -> None:
+        pass
--- strava_to_db.py
+++ strava_to_db.py
@@ -8,14 +8,16 @@ import mariadb
 import sys
 
 import database_export as dbe
+import strava_connector as sc
 
 # query Strava API with access token directly
 # http_header = {'Authorization': 'Bearer d623a206053610f20ba7c43f17794d38a8674ce8'}
 # http_response = requests.get('https://www.strava.com/api/v3/athlete', headers=http_header)
 
 auth_path = os.path.join(os.curdir, '.stravatokens', 'auth.cfg')
-client_path = os.path.join(os.curdir, '.stravatokens', 'client.cfg')
-dbconfig_path = os.path.join(os.curdir, '.dbconfig', 'db.cfg')
+# client_path = os.path.join(os.curdir, '.stravatokens', 'client.cfg')
+# dbconfig_path = os.path.join(os.curdir, '.dbconfig', 'db.cfg')
+config_path = os.path.join(os.curdir, '.appconf', 'strava_activity_extractor.cfg')
 
 authorization_url = 'http://www.strava.com/oauth/authorize'
 # TODO: is it https://www.strava.com/api/v3/oauth/token?
@@ -27,118 +29,70 @@ detailed_activity_url = 'https://www.strava.com/api/v3/activities/' # needs {id}
 # first run probably needs actual client authentication
 # http://www.strava.com/oauth/authorize?client_id=76303&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=read_all,profile:read_all,activity:read_all
 
-# create auth.cfg
+# create config
+appconf = configparser.ConfigParser()
+
+
 auth_config = configparser.ConfigParser()
 
 # create client.cfg
-client_config = configparser.ConfigParser()
-# client_config['Strava'] = {}
-# client_config['Strava']['client_Id'] = str(client_id)
-# client_config['Strava']['client_secret'] = client_secret
-# client_config['Strava']['client_code'] = client_code
-# client_config['Strava']['scope'] = scope
+# client_config = configparser.ConfigParser()
 
-# with open(client_path, 'w') as f:
-#     client_config.write(f)
-
-db_config = configparser.ConfigParser()
+# create modules
 db = dbe.DatabaseExport()
+strava = sc.StravaConnector(authconfigpath=auth_path)
 
 # load db.cfg
-if os.path.exists(dbconfig_path):
-    db_config.read(dbconfig_path, encoding='utf-8')
+if os.path.exists(config_path):
+    appconf.read(config_path, encoding='utf-8')
 else:
-    db_config['MariaDB'] = {}
-    db_config['MariaDB']['host'] = 'ENTER DATABASE URL HERE'
-    db_config['MariaDB']['port'] = '3306'
-    db_config['MariaDB']['name'] = 'ENTER DATABASE NAME HERE'
-    db_config['MariaDB']['user'] = 'ENTER DATABASE USER HERE'
-    db_config['MariaDB']['password'] = 'ENTER DATABASE USER PASSWORD HERE'
+    appconf['MariaDB'] = {}
+    appconf['MariaDB']['host'] = 'ENTER DATABASE URL HERE'
+    appconf['MariaDB']['port'] = '3306'
+    appconf['MariaDB']['name'] = 'ENTER DATABASE NAME HERE'
+    appconf['MariaDB']['user'] = 'ENTER DATABASE USER HERE'
+    appconf['MariaDB']['password'] = 'ENTER DATABASE USER PASSWORD HERE'
 
-    if not os.path.exists(os.path.dirname(dbconfig_path)):
-        os.mkdir(os.path.dirname(dbconfig_path))
-    with open(dbconfig_path, 'w') as f:
-        db_config.write(f)
+    if not os.path.exists(os.path.dirname(config_path)):
+        os.mkdir(os.path.dirname(config_path))
+    with open(config_path, 'w') as f:
+        appconf.write(f)
 
 # load client.cfg
-if os.path.exists(client_path):
-    client_config.read(client_path, encoding='utf-8')
-else:
-    client_config['Strava'] = {}
-    client_config['Strava']['client_Id'] = 'ENTER CLIENT ID HERE'
-    client_config['Strava']['client_secret'] = 'ENTER CLIENT SECRET HERE'
-    client_config['Strava']['client_code'] = 'ENTER CLIENT CODE FROM OAUTH REDIRECT HERE'
-    client_config['Strava']['scope'] = 'read_all,profile:read_all,activity:read_all'
+    appconf['Strava'] = {}
+    appconf['Strava']['client_id'] = 'ENTER CLIENT ID HERE'
+    appconf['Strava']['client_secret'] = 'ENTER CLIENT SECRET HERE'
+    appconf['Strava']['client_code'] = 'ENTER CLIENT CODE FROM OAUTH REDIRECT HERE'
+    appconf['Strava']['scope'] = 'read_all,profile:read_all,activity:read_all'
 
-    if not os.path.exists(os.path.dirname(client_path)):
-        os.mkdir(os.path.dirname(client_path))
-    with open(client_path, 'w') as f:
-        client_config.write(f)
+    with open(config_path, 'w') as f:
+        appconf.write(f)
 
-# check if auth.cfg already exists from previous authentication (if not, will be created automatically later)
+# check if auth.cfg already exists from previous authentication
 if os.path.exists(auth_path):
     auth_config.read(auth_path, encoding='utf-8')
 else:
-    # get new tokens from code
-    # TODO: also do this when 401 response
-    tokendata = {'client_id': client_config['Strava']['client_id'], 
-                'client_secret': client_config['Strava']['client_secret'],
-                'code': client_config['Strava']['client_code'],
-                'grant_type': 'authorization_code'
-                }
-    r = requests.post(url=token_url, data=tokendata)
-
-    # check for ok response (200)
-    if r.status_code == requests.codes.ok:
-        stravatokens = r.json()
-
-        # save tokens to config
-        auth_config['Strava'] = {}
-        auth_config['Strava']['access_token'] = stravatokens['access_token']
-        auth_config['Strava']['refresh_token'] = stravatokens['refresh_token']
-        auth_config['Strava']['expires_at'] = str(stravatokens['expires_at'])
-
-        # save auth config to file
-        if not os.path.exists(os.path.dirname(auth_path)):
-            os.mkdir(os.path.dirname(auth_path))
-        with open(auth_path, 'w') as f:
-            auth_config.write(f)
+    # if auth.cfg does not exist yet, create from code
+    strava.oauth_get_token_from_code(id=appconf['Strava']['client_id'],
+                                     secret=appconf['Strava']['client_secret'],
+                                     code=appconf['Strava']['client_code'])
 
 # check if token expired
 currepoch = int(time.time())
 if currepoch > int(auth_config['Strava']['expires_at'], base=10):
-    # refresh token
-    refreshdata = {'client_id': client_config['Strava']['client_id'],
-                   'client_secret': client_config['Strava']['client_secret'],
-                   'grant_type': 'refresh_token',
-                   'refresh_token': auth_config['Strava']['refresh_token']
-                   }
-    r = requests.post(url=token_url, data=refreshdata)
-
-    # check for ok response (200)
-    if r.status_code == requests.codes.ok:
-        stravatokens = r.json()
-
-        # update auth.cfg
-        # TODO: rework objectified
-        auth_config.set(section='Strava', option='access_token', value=stravatokens['access_token'])
-        auth_config.set(section='Strava', option='refresh_token', value=stravatokens['refresh_token'])
-        auth_config.set(section='Strava', option='expires_at', value=str(stravatokens['expires_at']))
-
-        # write changes of auth.cfg
-        if not os.path.exists(os.path.dirname(auth_path)):
-            os.mkdir(os.path.dirname(auth_path))
-        with open(auth_path, 'w') as f:
-            auth_config.write(f)
+    
+    # refresh tokens
+    strava.oauth_refresh_access_token(id=appconf['Strava']['client_id'],
+                                      secret=appconf['Strava']['client_secret'])
 
 # establish database connection
 dbconn : mariadb.Connection = None
 try:
-    dbconn = mariadb.connect(user=db_config['MariaDB']['user'],
-                             password=db_config['MariaDB']['password'],
-                             host=db_config['MariaDB']['host'],
-                             port=int(db_config['MariaDB']['port']),
-                             database=db_config['MariaDB']['name'])
+    dbconn = mariadb.connect(user=appconf['MariaDB']['user'],
+                             password=appconf['MariaDB']['password'],
+                             host=appconf['MariaDB']['host'],
+                             port=int(appconf['MariaDB']['port']),
+                             database=appconf['MariaDB']['name'])
 except mariadb.Error:
     # log.exception(f"Error connecting to MariaDB instance {dbconn.database} @ {dbconn.server_name}!")
     sys.exit(500)
@@ -160,9 +114,10 @@ if response.status_code == requests.codes.ok:
 
 # make activities request
 print(f"Current epoch: {currepoch}")
-params = {'before': currepoch,
+params = {#'before': currepoch,
+          'after': 0,
           'page': 1,
-          'per_page': 10
+          'per_page': 3
           }
 response = requests.get(url=activities_url, params=params, headers=header)
 
